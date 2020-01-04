31a78f2d32c677989c87d5fcb187566d4d91d8b7
Nick Herrick <nth.herrick@gmail.com>
2020-01-04 18:05:17 -0500

    added initial file structure and code

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('data'));
+app.use(express.static('public'));
+app.use(express.static('routing'));
