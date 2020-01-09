2d0a4919cf991a6b5143253724b6b2eadb0f32e2
Nick Herrick <nth.herrick@gmail.com>
2020-01-09 16:05:12 -0500

    changed initial friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,13 @@
 let friends = [
     {
-        name: "Person1",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+        name: "Itachi Uchiha",
+        photo:"http://images6.fanpop.com/image/photos/41900000/Itachi-Uchiha-itachi-uchiha-41946326-690-690.jpg",
+        scores: [5, 3, 5, 2, 2, 4, 2, 1, 5, 5]
     },
     {
-        name: "Person2",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+        name: "Naruto Uzumaki",
+        photo:"https://www.naruto-guides.com/wp-content/uploads/2019/05/naruto-uzumaki.jpg",
+        scores: [4, 4, 5, 1, 4, 2, 4, 3, 4, 1]
     }
 ];
 
