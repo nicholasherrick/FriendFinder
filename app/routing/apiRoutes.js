24c38744c290974a3812a347c9a31b28d552a1aa
Nick Herrick <nth.herrick@gmail.com>
2020-01-09 19:40:22 -0500

    majorly shorthanded find match code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,67 +7,21 @@ module.exports = (app) =>{
 
     app.post('/api/friends/', (req, res) =>{
         console.log('New user added to friends');
-
-        var userData = req.body;
-        var userScores = [];
-        var differences = [];
         
-        for(var i = 0; i < userData.scores.length; i++){
-            userData.scores[i] = parseInt(userData.scores[i]);
-            userScores.push(userData.scores[i]);
-        }
-
-        for(var i = 0; i < friends.length; i++){
-            for(var j = 0; j < friends[i].scores.length; j++){
-                differences.push(Math.abs(userData.scores[j] - friends[i].scores[j]));
-            }
-        }
-        chunkIt(differences, 10);
-        function chunkIt(array, size){
-            var differencesChunked = [];
-            for(var i = 0; i < array.length; i++){
-                var last = differencesChunked[differencesChunked.length - 1];
-                if (!last || last.length === size){
-                    differencesChunked.push([array[i]]);
-                }else{
-                    last.push(array[i]);
-                }
-            }
-            findMatch(differencesChunked);
-        }
-
-        function findMatch(diff){
-            var sumsArray = [];
-            for(i = 0; i < diff.length; i++){
-                var allDifferences = diff[i];
-                var sum = 0;
-                for(var j = 0; j < diff[i].length; j++){
-                    sum = sum + allDifferences[j];
-                }
-                sumsArray.push(sum);
-            }
-            getIndex(sumsArray);
-        }
+        var maxDifference = 100;
+        var potentialMatch
+        var client = req.body
         
-        function getIndex(sumsArray){
-            var index = 0;
-            var value = sumsArray[0];
-            for(var i = 1; i < sumsArray.length; i++){
-                if(sumsArray[i] < value){
-                    value = sumsArray[i];
-                    index = i;
-                }
+        friends.forEach(friend =>{
+            var scoreDifference = 0;
+            for(var i = 0; i < client.scores.length; i++){
+                scoreDifference += Math.abs(parseInt(friend.scores[i]) - parseInt(client.scores[i]));
             }
-            returnData(index);
-        }
-
-        function returnData(index){
-            for(var i = 0; i < friends.length; i++){
-                if(i === index){
-                    friends.push(req.body);
-                    res.json(friends[index]);
-                }
+            if(scoreDifference < maxDifference){
+                maxDifference = scoreDifference;
+                potentialMatch = friend;
             }
-        }
+        });
+       res.json(potentialMatch);
     });
 }
